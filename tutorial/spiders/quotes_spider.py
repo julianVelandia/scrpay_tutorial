8a5d652aafa57736e23a79b7631cca4eb763cade
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-11-14 11:52:29 -0500

    some changes

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -7,15 +7,15 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        '''
-        with open('resultados.txt', 'w', encoding='utf-8') as f:
-            f.write(quotes.text)
-        '''
-
-        yield {
-            'quotes' : quotes
+        
+        yield{
+            'title': title,
+            'quotes': quotes
         }
-        
-        
+    
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
        
